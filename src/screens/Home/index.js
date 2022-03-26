f3faa6d8a5d8090dc58fdb315df4503fdf4c48f0
Matias A. Khairallah <matias.khairallah@gmail.com>
2022-03-26 20:04:09 -0300

    my first commit

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,7 +2,8 @@ import React, { Component, useCallback } from 'react';
 import { 
     SafeAreaView,
     View, 
-    Text
+    Text,
+    Pressable
 } from 'react-native';
 import { styles } from './styles'
 
@@ -10,9 +11,36 @@ export default Home = () => {
 
     return(
         <SafeAreaView style={styles.container}>
-            <Text>
-                Home
-            </Text>
+            <View style={styles.mainCont}>
+                <View style={styles.rowCont}>
+                <Pressable style={[styles.buttonCont, {backgroundColor: 'red'}]}
+                 onPress={()=>alert('boton 1')}>
+                    <Text style={styles.tittle}>
+                        boton 1
+                    </Text>
+                </Pressable>
+                <Pressable style={[styles.buttonCont, {backgroundColor: 'blue'}]}
+                 onPress={()=>alert('boton 2')}>
+                    <Text style={styles.tittle}>
+                        boton 2
+                    </Text>
+                </Pressable>
+                </View>
+                <View style={styles.rowCont}>
+                <Pressable style={[styles.buttonCont, {backgroundColor: 'gray'}]}
+                 onPress={()=>alert('boton 3')}>
+                    <Text style={styles.tittle}>
+                        boton 3
+                    </Text>
+                </Pressable>
+                <Pressable style={[styles.buttonCont, {backgroundColor: 'green'}]}
+                 onPress={()=>alert('boton 4')}>
+                    <Text style={styles.tittle}>
+                        boton 4
+                    </Text>
+                </Pressable>
+                </View>
+            </View>         
         </SafeAreaView>
     )
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -6,5 +6,33 @@ const height = Dimensions.get('window').height;
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
       },
+      mainCont: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      rowCont: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      buttonCont: {
+       width: width/3,
+       height: width/3,
+       margin: 10,
+       borderRadius: 10,
+       alignItems: 'center',
+       justifyContent: 'center',
+      },
+      tittle: {
+        backgroundColor: 'lightblue',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 20
+      }
 })
